b693890fc16549406907c19459f83ac19e4ee460
Marco van Zwetselaar <io@zwets.it>
2021-08-24 09:19:07 +0300

    Upgrade all deps to latest

---
+++ test/test-04-interop.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+LC_ALL="C"
+
+BASE_NAME="$(basename "$0" .sh)"
+BASE_DIR="$(realpath "$(dirname "$0")")"
+
+export QAAP_DB_DIR="$BASE_DIR/databases"
+
+. "$BASE_DIR/functions.sh"
+
+# Pick a directory that works for you here
+TEST_DIR="${TEST_DIR:-/hpc/data/genomics/kcri/miseq/MiSeqOutput/150914_M02836_0007_000000000-AH6J0}"
+[ -d "$TEST_DIR" ] || { echo "$(basename "$0"): please set TEST_DIR" && exit 1; }
+
+make_output_dir
+run_qaap -x FastQC,FastQScreen,ReadsMetrics -v --platform MiSeq -o "$OUTPUT_DIR" "$TEST_DIR"
+check_output
+
